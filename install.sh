f47485b8a7fd4a09f24e81b915f5072822fda9e8
Peter Fry <pfry@pfry.remote.csb>
2017-05-11 14:44:10 +0100

    vagrant specific playbooks brought into vagrant project

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@ ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
 for host in node1.example.com node2.example.com node3.example.com; \
     do ssh-copy-id -i /root/.ssh/id_rsa.pub $host; \
     done
-cd /home/vagrant/ansible-playbook-fuse 
+cd /home/vagrant/ansible-playbook-fuse
+cp /vagrant/fuse-install-vagrant.yaml .
+cp /vagrant/fuse-deploy-bundle-vagrant . 
 ansible-playbook -i hosts fuse-install-vagrant.yaml --extra-vars "maven_repository_manager=http://10.0.2.2:8081/nexus/content/groups/public"
 
 
